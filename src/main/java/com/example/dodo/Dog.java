a1a2c8cbfb7a2cb05ba79ff895f7df0c0c5beec8
Anton <selkup-89@mail.ru>
2021-12-05 22:04:36 +0600

    Test

---
+++ src/main/java/com/example/dodo/Dog.java
@@ -0,0 +1,67 @@
+package com.example.dodo;
+
+public class Dog {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private Integer num;
+
+    public Dog(Integer id, String name, Integer age, Integer num) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", num=" + num +
+                '}';
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public Dog(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Dog(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
