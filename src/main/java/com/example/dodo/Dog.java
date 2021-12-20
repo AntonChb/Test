d02eb9341500a84bfa6403f4f5cadf656c8d7e82
AntonChb <selkup-89@mail.ru>
2021-12-20 23:47:56 +0600

    собрал в кучу все распиздяйство

--- src/main/java/com/example/dodo/Dog.java
+++ src/main/java/com/example/dodo/Dog.java
@@ -1,76 +1,25 @@
 package com.example.dodo;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-@Setter
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@NoArgsConstructor
 @Getter
+@Setter
 @ToString
+@AllArgsConstructor
 
 public class Dog {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
     private Integer age;
     private Integer num;
-
-    public Dog(Integer id, String name, Integer age, Integer num) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.num = num;
-    }
-
-    @Override
-    public String toString() {
-        return "Dog{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", num=" + num +
-                '}';
-    }
-
-    public Integer getNum() {
-        return num;
-    }
-
-    public void setNum(Integer num) {
-        this.num = num;
-    }
-
-    public Dog(Integer num) {
-        this.num = num;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Dog(String name, Integer age) {
-        this.name = name;
-        this.age = age;
-    }
 }
