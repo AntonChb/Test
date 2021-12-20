3b7fba13b673536203e93748a4059b5dc836c8b0
AntonChb <selkup-89@mail.ru>
2021-12-20 22:58:48 +0600

    добавил собак и некоторые проперти

--- src/main/java/com/example/dodo/DodoApplication.java
+++ src/main/java/com/example/dodo/DodoApplication.java
@@ -1,69 +1,15 @@
 package com.example.dodo;
 
-import com.sun.deploy.security.JarSignature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DodoApplication {
 
-    private JarSignature jdbcOperations;
-    int result;
-
     public static void main(String[] args) {
         SpringApplication.run(DodoApplication.class, args);
     }
 
-    Dog dog = null;
-
-    @GetMapping("/dog")
-    public Dog dog1(Long id) {
-        //serviceDog.getById(id);
-        return this.dog;
-    }
-
-
-    @PostMapping("/dog")
-    public Dog postDog(@RequestBody Dog dog) throws Exception {
-        dog.setId(3);
-        this.dog = dog;
-//        try {
-//            if (dog.getAge() > 20)
-//
-//        throw new Exception();
-//        }catch (Exception e)
-//        {
-//            ///logger
-//        }
-//        //serviceDog.save(dog);
-        return dog;
-    }
-
-    @PutMapping("/dog")
-    public Dog puttDog(@RequestBody Dog dog) {
-        dog.setId(3);
-        this.dog = dog;
-        return dog;
-    }
-
-    @DeleteMapping("/dog")
-    public String deleteDog() {
-        this.dog = null;
-        return "Файл удален";
-        //@PathVariable Long id
-        //serviceDog.delteById(id);
-
-    }
-
-    ///работаем в дев ветке
-    ///работаем в дев ветке еще
-
-    ///ты создал безопасоный вход по паролю
-
-    //ктото че то наебенил
 }
--- src/main/java/com/example/dodo/Dog.java
+++ src/main/java/com/example/dodo/Dog.java
@@ -1,5 +1,14 @@
 package com.example.dodo;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+
 public class Dog {
 
     private Integer id;
