e7395423a37e4ac7f232ee74d05b7a21eb8e78b7
AntonChb <selkup-89@mail.ru>
2022-01-14 20:58:57 +0600

    add manytomany pers-lakes2

--- src/main/java/com/example/dodo/controller/LakesController.java
+++ src/main/java/com/example/dodo/controller/LakesController.java
@@ -1,4 +1,66 @@
 package com.example.dodo.controller;
 
+import com.example.dodo.models.Lakes;
+import com.example.dodo.service.LakesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Controller
 public class LakesController {
+
+    @Autowired
+    private LakesService lakesService;
+
+
+    @Operation(
+            summary = "Вывод всех озер",
+            description = "Позволяет получить весь список озер"
+    )
+
+    @GetMapping(path = "/lakes")
+    public List<Lakes> getAllLakes() {
+        return lakesService.findAll();
+    }
+
+    @Operation(
+            summary = "Вывод озер по id",
+            description = "Позволяет оплучить озеро по id"
+    )
+    @GetMapping(path = "/lakesid")
+    public Lakes lakes(@RequestParam Integer id) {
+        return lakesService.getById(id);
+    }
+
+    @Operation(
+            summary = "Добавить новое озеро",
+            description = "Позволяет добавить новое озеро"
+    )
+    @PostMapping(path = "/lakes")
+    public Lakes addNewLakes(@RequestBody Lakes personal) {
+        return lakesService.save(personal);
+    }
+
+    @Operation(
+            summary = "Изменить данные о озере",
+            description = "Позволяет внести изменения об озере"
+    )
+    @PutMapping("/lakes")
+    public Lakes puttLakes(@RequestBody Lakes personal) {
+        return lakesService.save(personal);
+    }
+
+    @Operation(
+            summary = "Удалить озеро",
+            description = "Позволяет удалить озеро"
+    )
+    @DeleteMapping("/lakes")
+    public void deleteLakes(@Parameter(description = "Удалить озеро по id") @RequestParam Integer id) {
+        lakesService.deleteById(id);
+    }
 }
