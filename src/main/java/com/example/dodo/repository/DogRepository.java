ab7c81795bd5b82227efa61f7a3f12095d5d95d8
AntonChb <selkup-89@mail.ru>
2022-01-18 22:56:47 +0600

    zz

--- src/main/java/com/example/dodo/repository/DogRepository.java
+++ src/main/java/com/example/dodo/repository/DogRepository.java
@@ -2,19 +2,25 @@ package com.example.dodo.repository;
 
 import com.example.dodo.models.Dog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface DogRepository extends JpaRepository <Dog, Integer> {
+public interface DogRepository extends JpaRepository<Dog, Integer> {
 
-    @Query("SELECT DISTINCT name FROM Dog WHERE age > 10")
-    List <String> f (String name);
+    @Modifying
+    @Query(value = "UPDATE dog SET name = 'sad' WHERE name = 'Шарик'", nativeQuery = true)
+    List <String> f(String name);
 
 //    @Modifying
+//    @Query("UPDATE Note n SET n.title = ?1 WHERE n.id = ?2")
+//    int updateTitleById(String title, Long id);
 
-
+//    UPDATE FamilyMembers
+//    SET member_name='Andie Anthony'
+//    WHERE member_name='Andie Quincey';
 
 }
